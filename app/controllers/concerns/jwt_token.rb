197ce2005f4f128e9b4761ae7900b38d73d86c2c
davinderclerisy <davinder@clerisysolutions.com>
2023-09-12 18:21:19 +0530

    init

--- app/controllers/concerns/jwt_token.rb
+++ app/controllers/concerns/jwt_token.rb
@@ -1,8 +1,8 @@
 require "jwt"
 
 module JwtToken extend ActiveSupport::Concern
-    SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
-      
+    SECRET_KEY = Rails.application.secret_key_base. to_s
+
     def self.jwt_encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
         JWT.encode(payload, SECRET_KEY)
